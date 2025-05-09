07488d093dacc653632eb90023a5560012f16354
Reproductores <39553326+cesar42@users.noreply.github.com>
2025-05-09 08:56:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
     // Detect if device is iOS
     if (!isIos()) {
-        installBtn.app.js.display = 'block';
+        installBtn.app.js.display = 'app';
     }
 
     installBtn.addEventListener('click', () => {
